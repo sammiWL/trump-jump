35e04afd98c3ac07b280ef2e69ce6f64d8757bf3
Ariel Levy <ariel.s.levy1@gmail.com>
2016-03-06 20:44:49 -0500

    trump face

--- svg.js
+++ svg.js
@@ -1,6 +1,39 @@
 console.log('ayy');
 
 var pic=document.getElementById('america');
+var trump = document.getElementById('trump');
+var score = document.getElementById('score');
+
+var intervalID;
+
+var setup = function setup() {
+	clearInterval(intervalID);
+	score.innerHTML = '0';
+
+	var scoreNum = 0;
+
+	var up = true;
+	var trumpX = pic.width.baseVal.value / 2;
+	var trumpY = pic.height.baseVal.value / 2;
+	var jumpLimit = 150;
+	var curJump = 0;
+
+	var trumpJump = function trumpJump() {
+		trump.setAttribute('x', trumpX);
+		trump.setAttribute('y', trumpY);
+
+		if ( up ) { trumpY -= 1; curJump += 1 }
+		else { trumpY += 1 }
+
+		if ( curJump >= jumpLimit ) { up = false; }
+
+		if ( trumpY > pic.height.baseVal.value ) { console.log("Game Over") }
+
+		document.onmousemove {}
+
+	}
+	intervalID = window.setInterval( trumpJump, 5 );
+};
 
 var p1=document.createElementNS('http://www.w3.org/2000/svg','rect');
 
@@ -16,3 +49,5 @@ pic.appendChild(p1);
 var platforms=[]
 platforms.push(p1);
 console.log(platforms);
+
+setup();
