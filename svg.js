4dda96a9b1474e4d17e775aad518d94462235eb2
Sammi <sammiwuleung@gmail.com>
2016-03-07 14:05:52 -0500

    changes

--- svg.js
+++ svg.js
@@ -23,31 +23,33 @@ var setup = function setup(e) {
     pic.addEventListener('mousemove', function (e) {
 	trumpX = e.clientX - 55;
     });
-    var p1, i;
+    var p, i, j;
 
     for (i = 0; i < 4; i++) {
-	p1=document.createElementNS('http://www.w3.org/2000/svg','rect');
-	console.log(i);
-	p1.setAttribute('x',91 * i);
-	p1.setAttribute('y',585);
-	p1.setAttribute('fill','#ffd700');
-	p1.setAttribute('stroke','#c78201');
-	p1.setAttribute('width',70);
-	p1.setAttribute('height',15);
-	platforms.push(p1);
-	pic.appendChild(p1);
-	console.log(platforms);
-	rect = pic.createSVGRect();
-	rect.x = 91 * i + 30 ;
-	rect.y = 585 + 10;
-	rect.height = 15;
-	rect.width = 20;
-	rectA.push(rect);
-	console.log(rectA);
+	for (j = 0; j < 4; j++) {
+	    p = document.createElementNS('http://www.w3.org/2000/svg','rect');
+	    console.log(i);
+	    p.setAttribute('x',91 * i);
+	    p.setAttribute('y',585 - (j * 150));
+	    p.setAttribute('fill','#ffd700');
+	    p.setAttribute('stroke','#c78201');
+	    p.setAttribute('width',70);
+	    p.setAttribute('height',15);
+	    platforms.push(p);
+	    pic.appendChild(p);
+	    console.log(platforms);
+	    rect = pic.createSVGRect();
+	    rect.x = 91 * i + 30 ;
+	    rect.y = 585 + 10 - (j * 100);
+	    rect.height = 15;
+	    rect.width = 20;
+	    rectA.push(rect);
+	    console.log(rectA);
+	}
     }
    
 			   
-    var trumpJump = function trumpJump() {
+    var trumpJump = function trumpxJump() {
 	
 	trump.setAttribute('x', trumpX);
 	trump.setAttribute('y', trumpY);
@@ -66,32 +68,111 @@ var setup = function setup(e) {
 	}
 
 	if (!up) {
-	    if (check_platform()) {
+	    var change = check_platforms();
+	    console.log(change + "CHANGE");
+	    if (change > 0) {
 		console.log("I MADE IT GREAT AGAIN");
 		up=!up;
 		curJump=0;
+		move_platforms(change);
+		clean_platforms();
+		create_platforms();
 	    }
+
+	    //create_platforms();
 	}
+
 	//console.log(curJump);
     }
     intervalID = window.setInterval( trumpJump, 5 );
-
+    
 };
 
-var check_platform = function check_platform() {
+var create_platforms = function create_platform() {
+    var x, y, p, i;
+    var anyless50 = false;
     var count = 0;
-    var	on = false;
+    var currentY = 0;
+    while (count < platforms.length) {
+	currentY = parseInt(platforms[count].getAttribute('y'));
+	console.log(currentY);
+	console.log("WHYYY");
+	//console.log(currentY);
+	anyless50 = anyless50 || (currentY < 50);
+	if (anyless50) break;
+	count++;
+    }
+
+    if (!anyless50) {
+	p=document.createElementNS('http://www.w3.org/2000/svg','rect');
+	console.log(i);
+	x = Math.floor(Math.random() * 330);
+	y = Math.floor(Math.random() * 50) - 50;
+	p.setAttribute('x',x);
+	p.setAttribute('y',y);
+	p.setAttribute('fill','#ffd700');
+	p.setAttribute('stroke','#c78201');
+	p.setAttribute('width',70);
+	p.setAttribute('height',15);
+	platforms.push(p);
+	pic.appendChild(p);
+	console.log(platforms);
+	rect = pic.createSVGRect();
+	rect.x = x + 30 ;
+	rect.y = y + 10;
+	rect.height = 15;
+	rect.width = 20;
+	rectA.push(rect);
+	console.log(rectA);
+    }
+}
+
+var check_platforms = function check_platforms() {
+    var count = 0;
+    var	change = 0;
+    
     //console.log("HI");
     while (count < rectA.length) {
-	on = on || (pic.checkIntersection(trump, rectA[count]));
+	if (pic.checkIntersection(trump, rectA[count])) {
+	    change = parseInt(platforms[count].getAttribute('y'));
+	    break;
+	}
 	//console.log(on);
 	count++;
     }
-    return on;
+    console.log(change + "THIS IS THE CHANGE");
+    return change;
 }
 
-var move_platforms = function move_platforms() {
-    
+var move_platforms = function move_platforms(changeY) {
+    var count = 0;
+    var prevY = 0;
+    while (count < platforms.length) {
+	prevY = parseInt(platforms[count].getAttribute('y'));
+	//console.log(prevY + changeY);
+	platforms[count].setAttribute('y', prevY + changeY);
+	//console.log(platforms);
+	rectA[count].y = prevY + changeY + 10;
+	count++;
+    }
+}
+
+var clean_platforms = function clean_platforms() {
+    var count = 0;
+    var currentY = 0;
+    while (count < platforms.length) {
+	currentY = parseInt(platforms[count].getAttribute('y'));
+	console.log(currentY);
+	if (currentY > 600) {
+	    platforms.shift();
+	    rectA.shift();
+	    console.log(platforms);
+	}
+	else {
+	    break;
+	}
+	
+    }
 }
 
 setup();
